54d346300a7bd88ddc8e944374e9f0fcf8c1752b
muhamadgibran16 <muhamadgibran2333@gmail.com>
2023-06-28 21:01:05 +0700

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+/* global bootstrap: false */
+// (function () {
+//   'use strict'
+//   var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+//   tooltipTriggerList.forEach(function (tooltipTriggerEl) {
+//     new bootstrap.Tooltip(tooltipTriggerEl)
+//   })
+// })()
+    // const container = document.querySelector('.container');
+    // const items = container.querySelectorAll('.order');
+
+    // Ubah urutan elemen
+    // items[0].style.order = '2';
+    // items[1].style.order = '1';
+    // items[2].style.order = '3';
+
+    // $(document).ready(function () {
+    //   if ($(window).width() < 528) {
+    //     $('#order-1').css('order', '2');
+    //     $('#order-2').css('order', '1');
+    //     } else {
+    //       $('#order-1').css('order', '1');
+    //       $('#order-2').css('order', '2');
+    //   }
+    // });
